3fb41764ad77a6d848b32875e08ba176f685315a
AlekseySteblivets <aleksey.steblivets@gmail.com>
2022-05-25 22:57:56 +0300

    add modal-bag

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,42 @@
+import { Component } from "react";
+import { createPortal } from "react-dom";
+
+import s from "./Modal.module.scss";
+
+const modalRoot = document.querySelector("#modal-root");
+
+class Modal extends Component {
+  componentDidMount() {
+    console.log("componentDidMount");
+
+    window.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+    window.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  handleKeyDown = (e) => {
+    if (e.code === "Escape") {
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = (e) => {
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <div className={s.modalBackdrop} onClick={this.handleBackdropClick}>
+        <div className={s.modalContent}>{this.props.children}</div>
+      </div>,
+      modalRoot
+    );
+  }
+}
+
+export default Modal;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import Modal from "../Modal/Modal";
+
 import logo from "../../assets/img/Brand-icon.png";
 import emptyCartImg from "../../assets/img/empty-cart.svg";
 import vector from "../../assets/img/vector.svg";
@@ -6,9 +8,18 @@ import vector from "../../assets/img/vector.svg";
 import styles from "./Navigation.module.scss";
 
 export default class Navigation extends Component {
-  state = {};
+  state = {
+    showModal: false,
+  };
+
+  togleModal = () => {
+    this.setState((state) => ({
+      showModal: !state.showModal,
+    }));
+  };
 
   render() {
+    const { showModal } = this.state;
     return (
       <nav className={styles.navigation}>
         <ul className={styles.menu}>
@@ -42,15 +53,70 @@ export default class Navigation extends Component {
           <p className={styles.textMoney}>$</p>
           <img alt="vector" src={vector} />
 
-          <a href="/">
+          <button
+            type="button"
+            onClick={this.togleModal}
+            className={styles.buttonOnBag}
+          >
             <img
               alt="emptyCartImg"
               src={emptyCartImg}
               className={styles.emptyCartImg}
             />
-          </a>
+          </button>
         </div>
+        {showModal && (
+          <Modal onClose={this.togleModal}>
+            <h1> Привет это контент модалки как children</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <button type="button" onClick={this.togleModal}>
+              Закрыть модалку
+            </button>
+          </Modal>
+        )}
       </nav>
     );
   }
 }
+
+// class App extends Component {
+//   state = {
+//     showModal: false,
+//   };
+
+//   togleModal = () => {
+//     this.setState((state) => ({
+//       showModal: !state.showModal,
+//     }));
+//   };
+
+//   render() {
+//     const { showModal } = this.state;
+//     return (
+//       <div>
+//         <button type="button" onClick={this.togleModal}>
+//           Open Modal
+//         </button>
+//         {showModal && (
+//           <Modal onClose={this.togleModal}>
+//             <h1> Привет это контент модалки как children</h1>
+//             <p>
+//               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+//               enim ad minim veniam, quis nostrud exercitation ullamco laboris
+//               nisi ut aliquip ex ea commodo consequat.
+//             </p>
+//             <button type="button" onClick={this.togleModal}>
+//               Закрыть модалку
+//             </button>
+//           </Modal>
+//         )}
+//       </div>
+//     );
+//   }
+// }

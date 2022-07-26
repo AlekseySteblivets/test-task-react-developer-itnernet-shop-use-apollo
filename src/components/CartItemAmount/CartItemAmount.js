5d3a6dbf2c0843a7676039c37c1cb224f13f8169
AlekseySteblivets <aleksey.steblivets@gmail.com>
2022-07-26 11:43:35 +0300

    fixed styles(color) which use on Select, fixed that The cart item quantity cannot be less than, fixed color and opacity produc out of stock, fixed that Some images are distorted

--- src/components/CartItemAmount/CartItemAmount.js
+++ src/components/CartItemAmount/CartItemAmount.js
@@ -1,12 +1,12 @@
-import { Component } from "react";
-import cn from "classnames";
+import { Component } from 'react';
+import cn from 'classnames';
 
-import { graphql } from "@apollo/client/react/hoc";
-import { client } from "../../api/base/apolloClient";
+import { graphql } from '@apollo/client/react/hoc';
+import { client } from '../../api/base/apolloClient';
 
-import { READ_GET_PRODUCT_INTO_CART } from "../../api/cache/getProductIntoCart";
+import { READ_GET_PRODUCT_INTO_CART } from '../../api/cache/getProductIntoCart';
 
-import styles from "./CartItemAmount.module.scss";
+import styles from './CartItemAmount.module.scss';
 
 class CartItemAmount extends Component {
   state = {
@@ -16,7 +16,7 @@ class CartItemAmount extends Component {
   componentDidMount() {
     if (this.props.productId) {
       let product = this.props.data.productIntoCart.filter(
-        (product) => product.id === this.props.productId
+        product => product.id === this.props.productId,
       );
       this.setState({ amount: product[0].numbersItem });
     }
@@ -27,32 +27,32 @@ class CartItemAmount extends Component {
       {
         query: READ_GET_PRODUCT_INTO_CART,
       },
-      (data) => ({
-        productIntoCart: data.productIntoCart.map((product) => ({
+      data => ({
+        productIntoCart: data.productIntoCart.map(product => ({
           ...product,
           numbersItem:
             product.id === this.props.productId
               ? this.state.amount
               : product.numbersItem,
         })),
-      })
+      }),
     );
   };
 
   handleIncrement = () => {
     this.setState(
-      (prevState) => ({ amount: prevState.amount + 1 }),
-      () => this.updateQuery()
+      prevState => ({ amount: prevState.amount + 1 }),
+      () => this.updateQuery(),
     );
   };
 
   handleDecrement = () => {
-    if (this.state.amount === 0) {
-      this.setState({ amount: 0 }, () => this.updateQuery());
+    if (this.state.amount === 1) {
+      this.setState({ amount: 1 }, () => this.updateQuery());
     } else {
       this.setState(
-        (prevState) => ({ amount: prevState.amount - 1 }),
-        () => this.updateQuery()
+        prevState => ({ amount: prevState.amount - 1 }),
+        () => this.updateQuery(),
       );
     }
   };
@@ -85,7 +85,7 @@ class CartItemAmount extends Component {
 }
 
 export default graphql(READ_GET_PRODUCT_INTO_CART, {
-  options: (props) => ({
-    fetchPolicy: "cache-only",
+  options: props => ({
+    fetchPolicy: 'cache-only',
   }),
 })(CartItemAmount);
